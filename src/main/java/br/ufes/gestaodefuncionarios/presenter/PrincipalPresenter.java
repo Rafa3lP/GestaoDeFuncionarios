5a5f78d3fb4200dbcfabc74470ca2e5c862fc0e0
R@fa3lP <rafaelprenholato@hotmail.com>
2022-01-04 17:15:38 -0300

    first commit

---
+++ src/main/java/br/ufes/gestaodefuncionarios/presenter/PrincipalPresenter.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufes.gestaodefuncionarios.presenter;
+
+import br.ufes.gestaodefuncionarios.view.PrincipalView;
+
+/**
+ *
+ * @author Rafael
+ */
+public class PrincipalPresenter {
+    private PrincipalView view;
+    
+}
